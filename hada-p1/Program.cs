a2e40d79350a2fde70c653ef7a402e9ce43c734b
erika <emp91@alu.ua.es>
2020-02-10 20:40:25 +0100

    Added Program todo

--- hada-p1/Program.cs
+++ hada-p1/Program.cs
@@ -8,8 +8,43 @@ namespace hada_p1
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            string respuesta = "";
+            do
+            {
+                Console.WriteLine("¿De que unidad quiere partir?(minutos/segundos)");
+                string unidad = Console.ReadLine();
+                Console.WriteLine("Cantidad:");
+                string cantidad = Console.ReadLine();
+                if (unidad == "minutos")
+                {
+                    double cantidad1 = double.Parse(cantidad);
+                    double total1 = HadaP1.Minutes2Seconds(cantidad1);
+                    Console.WriteLine("Cantidad de segundos:");
+                    Console.WriteLine(total1);
+                }
+                else
+                {
+                    if (unidad == "segundos")
+                    {
+                        double cantidad1 = double.Parse(cantidad);
+                        double total = HadaP1.Seconds2Minutes(cantidad1);
+                        Console.WriteLine("Cantidad de minutos:");
+                        Console.WriteLine(total);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error, no está esa opcion");
+                    }
+
+                }
+                Console.WriteLine("¿Tiene otra consulta?(s/n)");
+                respuesta = Console.ReadLine();
+
+            } while (respuesta != "n");
         }
+
     }
 }
