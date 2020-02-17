bfd6bb66f784944827d4592d443fcd57e1730456
erika <emp91@alu.ua.es>
2020-02-17 17:48:46 +0100

    Modificar el main para el nuevo metodo

--- hada-p1/Program.cs
+++ hada-p1/Program.cs
@@ -9,12 +9,16 @@ namespace hada_p1
     class Program
     {
 
+        /// <summary>
+        /// Modificamos el main para tener la opcion de las horas
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             string respuesta = "";
             do
             {
-                Console.WriteLine("¿De que unidad quiere partir?(minutos/segundos)");
+                Console.WriteLine("¿De que unidad quiere partir?(minutos/segundos/horas)");
                 string unidad = Console.ReadLine();
                 Console.WriteLine("Cantidad:");
                 string cantidad = Console.ReadLine();
@@ -36,7 +40,16 @@ namespace hada_p1
                     }
                     else
                     {
-                        Console.WriteLine("Error, no está esa opcion");
+                        if (unidad == "horas")
+                        {
+                            double cantidad1 = double.Parse(cantidad);
+                            double total = HadaP1.Hours2Minutes(cantidad1);
+                            Console.WriteLine("Cantidad de minutos:");
+                            Console.WriteLine(total);
+                        }
+                        else {
+                            Console.WriteLine("Error, no está esa opcion");
+                        }
                     }
 
                 }
